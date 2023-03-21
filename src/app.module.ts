aa7cbd96d2ac511d6062b7933b86528d54a08e86
Oleg Zubkov <76254867+olegz7@users.noreply.github.com>
2023-03-21 19:17:50 +0200

    setup typeorm and db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db',
+      port: 3306,
+      username: 'dbuser',
+      password: 'dbuser',
+      database: 'ambassador',
+      // entities may be added 1 by 1 here: 
+      // entities: [],
+      // but instead we can use (do not use on production)
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
